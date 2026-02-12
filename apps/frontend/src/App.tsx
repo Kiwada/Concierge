f2e45c6a372ea00d26ef88099860948c6e9d98be
Kiwada <mamakaiofrep@gmail.com>
2026-02-11 23:20:46 -0300

    chore: adjust section order in app layout

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,8 +2,7 @@ import Banner from "./components/Banner";
 import ChatAssistant from "./components/ChatAssistant";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import MovieSection from "./components/MovieSection";
-import Newsletter from "./components/Newsletter";
+
 import TourismShowcase from "./components/TourismShowcase/TourismShowcase";
 
 function App() {
